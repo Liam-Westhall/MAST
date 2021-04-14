e469633174459c1fb297890a888c475c09e40441
michael5morales <michael5morales@aol.com>
2021-04-14 15:38:55 -0400

    Enrollment Trends

--- client/src/GPDScreens/NavbarGPD.js
+++ client/src/GPDScreens/NavbarGPD.js
@@ -19,7 +19,7 @@ class NavbarGPD extends Component{
             extendWith={<ul className="tabs tabs-transparent">
                 <li class="tab white-text"><a target="_self" href="/manage_students_gpd">Manage Students</a></li>
                 <li class="tab white-text"><a target="_self" href="/upload_file_gpd">Upload File</a></li>
-                <li class="tab white-text"><a target="_self" href="/manage_students_gpd">View Enrollment Trends</a></li>
+                <li class="tab white-text"><a target="_self" href="/enrollment_trends_gpd">View Enrollment Trends</a></li>
                 <li class="tab white-text"><a target="_self" href="/manage_students_gpd">Sugggest Course Plan</a></li></ul>}
             options={{edge: 'left', preventScrolling: true}}>
             <NavItem>Profile</NavItem>
---
+++ server/routes/api/courses.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const stream = require('stream')
+const fs = require('fs')
+const csv = require('csv-parser')
+const {Op, json} = require("sequelize")
+
+const {Course, Degree, Student, User, Student_Course} = require('../../models')
+const db = require('../../models')
+const bodyParser = require("body-parser");
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    res.send("Test")
+})
+
+router.post('/courselist', async (req, res) => {
+    try {
+        const {department, courselist, semesters} = req.body;
+
+        let courses = await Course.findAll({
+            attributes: ['courseNumber', 'semester', 'totalStudents'],
+            where: { 
+                department: department,
+                semester: {[Op.in]: semesters.split(',') }
+            },
+            raw: true
+        })
+
+        console.log(courses)
+        res.send(courses)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("COURSE LIST ERR")
+    }
+    
+})
+
+module.exports = router;
