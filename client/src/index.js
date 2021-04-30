e8540dd8b477e47be5930f00dde15c37ed56f5bc
michael5morales <michael5morales@aol.com>
2021-04-30 16:31:03 -0400

    client host bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
 import 'materialize-css/dist/css/materialize.min.css'
+
+axios.defaults.baseURL = "https://mast-null-pointers.herokuapp.com";
 
 ReactDOM.render(
   <React.StrictMode>
