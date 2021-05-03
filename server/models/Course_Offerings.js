c137348d1c3953676b48c7e3a9b63c2ef5290089
michael5morales <michael5morales@aol.com>
2021-05-03 16:54:53 -0400

    Scrape Course Info

--- server/models/Course_Offerings.js
+++ server/models/Course_Offerings.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
         },
         
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
 
